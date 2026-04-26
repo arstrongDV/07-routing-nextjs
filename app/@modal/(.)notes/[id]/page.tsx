1fe103e8c2b8e0d1049d26e8b204ffcc0732a27d
arstrong <arsendacenko89@gmail.com>
2026-04-26 02:01:21 +0200

    03-updation

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Modal from "@/components/Modal/Modal";
-import NotePreview from "./NotePreview";
+import NotePreview from "./NotePreview.client";
 import { fetchNote } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
